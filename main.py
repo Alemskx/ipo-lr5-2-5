733132e1d8d9c58dc651926476ebfbd3b676a975
Alex <alemskx@gmail.com>
2024-10-24 00:07:22 +0300

    задание 5

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 str = input("Введи строку:")
-kolvo = 0
+count = 0
 str1=str.lower()
 for i in str1:
     if i =="и":
-        kolvo +=1
-print("Количество букв и:",kolvo)
+        count +=1
+print("Количество букв и:",count)
